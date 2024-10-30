42a0a5ac100fd8c6231b132e1c9929aba7c99265
KutayTasel <148221318+KutayTasel@users.noreply.github.com>
2024-10-30 09:40:46 +0300

    Update app.ts

--- leaderboard-backend/src/app.ts
+++ leaderboard-backend/src/app.ts
@@ -4,17 +4,20 @@ import rateLimit from "express-rate-limit";
 import cors from "cors";
 
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 dakika
+  windowMs: 15 * 60 * 1000,
   max: 1000,
   message: "Too many requests, please try again later.",
 });
 
 const createApp = () => {
   const app = express();
-
   app.use(
     cors({
-      origin: ["http://localhost:3000", "https://gameleaderboard.com"],
+      origin: [
+        "http://localhost:3000",
+        "http://51.20.106.162:3000",
+        "http://www.gameleaderboarddemoooo.com",
+      ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
@@ -23,11 +26,6 @@ const createApp = () => {
   app.use(express.json());
 
   app.use("/api/", apiLimiter);
-
-  app.get(
-    "/api/leaderboard/player/:playerId/rank",
-    leaderboardController.getPlayerRank
-  );
 
   app.get("/api/leaderboard/search", leaderboardController.searchPlayers);
 
@@ -46,6 +44,11 @@ const createApp = () => {
     "/api/leaderboard/all",
     leaderboardController.getAllPlayersLeaderboard
   );
+  app.post(
+    "/api/leaderboard/refresh-earnings",
+    leaderboardController.refreshEarnings
+  );
+
   app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
     console.error(err.stack);
     res
