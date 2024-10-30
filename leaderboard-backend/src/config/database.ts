cc8e7e14a2ef75c920e88b0fbe55389354458dbb
KutayTasel <148221318+KutayTasel@users.noreply.github.com>
2024-10-30 09:31:21 +0300

    Update database.ts

--- leaderboard-backend/src/config/database.ts
+++ leaderboard-backend/src/config/database.ts
@@ -1,30 +1,25 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-// .env dosyasındaki veritabanı bilgilerini kullanmak için dotenv'i yapılandırın
 dotenv.config();
 
-// Sequelize örneğini oluşturuyoruz, bu MySQL ile bağlantıyı kuracak
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "leaderboard_db", // Veritabanı adı
-  process.env.DB_USER || "root", // Kullanıcı adı
-  process.env.DB_PASSWORD || "", // Şifre
+  process.env.DB_NAME || "leaderboard_db",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "mysql", // MySQL ile çalıştığımızı belirtiyoruz
+    host: process.env.DB_HOST || "51.20.78.86",
+    dialect: "mysql",
     port: Number(process.env.DB_PORT) || 3306,
-    logging: false, // Konsolda SQL sorgularını görmemek için 'false' yapıyoruz
+    logging: false,
   }
 );
 
-// Bağlantının başarılı olup olmadığını kontrol edelim
 export const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
     console.log("MySQL bağlantısı başarılı!");
-
-    // Player tablosunu oluştur veya güncelle
-    await sequelize.sync(); // Bu, tüm modellerin tablolarını oluşturur
+    await sequelize.sync();
   } catch (error) {
     console.error("Veritabanı bağlantısı başarısız:", error);
   }
