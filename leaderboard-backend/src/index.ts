43f3f824b9312367245f1e81562c84221d9ec95c
KutayTasel <148221318+KutayTasel@users.noreply.github.com>
2024-10-30 09:41:01 +0300

    Update index.ts

--- leaderboard-backend/src/index.ts
+++ leaderboard-backend/src/index.ts
@@ -3,28 +3,19 @@ import { connectToDatabase } from "./config/database";
 import { initRedisConnection } from "./config/redis";
 import Player from "./models/player";
 import createApp from "./app";
-import seedData from "./data/seed"; // Seed fonksiyonunu dahil ediyoruz
+import seedData from "./data/seed";
 
-// .env dosyasını yüklemek için dotenv'i yapılandırıyoruz
 dotenv.config();
 
 (async () => {
   try {
-    // MySQL ve Redis bağlantısını başlatıyoruz
     await connectToDatabase();
     await initRedisConnection();
+    await Player.sync();
+    await seedData();
 
-    // Veritabanı modellerini senkronize ediyoruz
-    await Player.sync(); // Burada tabloların oluşturulmasını sağlıyoruz
-
-    // Seed verilerini ekliyoruz
-    await seedData(); // Seed verileri burada çalıştırılıyor
-
-    // Express uygulamasını oluşturuyoruz
     const app = createApp();
     const port = process.env.PORT || 3001;
-
-    // Express sunucusunu başlatıyoruz
     app.listen(port, () => {
       console.log(`Sunucu ${port} numaralı portta çalışıyor.`);
     });
